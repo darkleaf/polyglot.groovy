17eba9e36adceb3b05672bcfe0d707e099920e56
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-03-16 16:15:33 +0400

    ideas

--- dev/darkleaf/clj_groovy/benchmark/collection/gr_2.groovy
+++ dev/darkleaf/clj_groovy/benchmark/collection/gr_2.groovy
@@ -5,9 +5,9 @@ class gr_2 extends AFunction {
   static final IFn deref = var 'clojure.core', 'deref'
 
   // эти Var не будут меняться, и можем сразу их задерефить
-  static final IFn mapv = (IFn) var('clojure.core', 'mapv').with(deref)
-  static final IFn inc  = (IFn) var('clojure.core', 'inc' ).with(deref)
-  static final IFn str  = (IFn) var('clojure.core', 'str' ).with(deref)
+  static final IFn mapv = (IFn) (var 'clojure.core', 'mapv' with deref)
+  static final IFn inc  = (IFn) (var 'clojure.core', 'inc'  with deref)
+  static final IFn str  = (IFn) (var 'clojure.core', 'str'  with deref)
 
   def invoke(data) {
     data
