f642514becd43e1f41f8c1513d146fafaee63963
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-03-16 16:21:37 +0400

    protocol with meta

--- dev/darkleaf/clj_groovy/benchmark/reify.clj
+++ dev/darkleaf/clj_groovy/benchmark/reify.clj
@@ -4,6 +4,7 @@
    [darkleaf.clj-groovy.core :as g]))
 
 (defprotocol P
+  :extend-via-metadata true
   (a [this]))
 
 (g/defobject instance)
@@ -16,22 +17,36 @@
     (a [_]
       42)))
 
+(defn instance-clj2 []
+  (with-meta {}
+    {`a (fn [_] 42)}))
+
 (comment
   (a (instance))
   (a (instance-clj))
+  (a (instance-clj2))
 
   ;; reflection.Proxy :'(
   (c/quick-bench (a (instance)))
-  ;; Execution time mean : 215,461238 ns
+  ;; Execution time mean : 182,612822 ns
 
   (c/quick-bench (a (instance-clj)))
-  ;; Execution time mean : 5,336916 ns
+  ;; Execution time mean : 4,574165 ns
+
+  (c/quick-bench (a (instance-clj2)))
+  ;; Execution time mean : 45,350116 ns
+
+
 
   (let [obj (instance)]
     (c/quick-bench (a obj)))
-  ;; Execution time mean : 26,407025 ns
+  ;; Execution time mean : 23,560649 ns
 
   (let [obj (instance-clj)]
     (c/quick-bench (a obj)))
-  ;; Execution time mean : 5,073003 ns
+  ;; Execution time mean : 4,564991 ns
+
+  (let [obj (instance-clj2)]
+    (c/quick-bench (a obj)))
+  ;; Execution time mean : 79,675511 ns
   ,,,)
