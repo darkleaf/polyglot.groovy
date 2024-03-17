fdd3fbfc2c464e3393908c4dd49beab473483df3
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-03-17 17:12:56 +0400

    groovy

---
+++ test/darkleaf/clj_groovy/groovy_test/ClassWithMethod.groovy
@@ -0,0 +1,7 @@
+package darkleaf.clj_groovy.groovy_test
+
+class ClassWithMethod {
+  def myMethod() {
+    "my value"
+  }
+}
---
+++ test/darkleaf/clj_groovy/groovy_test/ClassWithMethodMissing.groovy
@@ -0,0 +1,7 @@
+package darkleaf.clj_groovy.groovy_test
+
+class ClassWithMethodMissing {
+  def methodMissing(String name, Object args) {
+    [name, args.toList()]
+  }
+}
